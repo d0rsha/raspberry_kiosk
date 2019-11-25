68288d20369a079d15133f9cb59034c3c5dc66f2
Anders Vrethem <dersi_91@hotmail.com>
2019-11-25 13:31:10 +0100

    Update kiosk.sh

--- kiosk.sh
+++ kiosk.sh
@@ -10,7 +10,7 @@ unclutter -idle 0.5 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk --ignore-certificate-errors --disable-restore-session-state https://www.latlmes.com/ & 
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk --ignore-certificate-errors --disable-restore-session-state https://www.latlmes.com/breaking/your-sensational-news-headline-here-3 & 
 
 while true; do
    	WID=$(xdotool search --onlyvisible --class chromium|head -1)
