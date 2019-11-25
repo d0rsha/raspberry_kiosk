59227ef7ffbb93e3b77784a4159cf99923067b5f
Anders Vrethem <dersi_91@hotmail.com>
2019-11-25 23:22:55 +0100

    Update first_boot.sh

--- first_boot.sh
+++ first_boot.sh
@@ -4,10 +4,12 @@ sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y 
 sudo apt-get autoremove -y 
 sudo apt-get autoclean -y 
+apt list unclutter
 sudo apt-get install -y unclutter
+apt list sed
 sudo apt-get install -y sed
+apt list xdotool
 sudo apt-get install -y xdotool
 sudo raspi-config --expand-rootfs
-chromium-browser & 
+chromium-browser https://google.com
 echo "First boot ended gracefully"
-
