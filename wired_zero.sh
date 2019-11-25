520ef57d14e8718effe29814749d0f3be4a723cf
Anders Vrethem <dersi_91@hotmail.com>
2019-11-26 00:51:20 +0100

    Update wired_zero.sh

--- wired_zero.sh
+++ wired_zero.sh
@@ -13,9 +13,14 @@ wget https://raw.githubusercontent.com/d0rsha/raspberry_kiosk/master/splash.png 
 sudo mv splash.png /usr/share/plymouth/themes/pix
 
 sudo echo "disable_splash=1" >> /boot/config.txt
+sudo echo "# Disable under-voltage warning" >> /boot/config.txt
+sudo echo "avoid_warnings=1" >> /boot/config.txt
 sudo echo "splash quiet plymouth.ignore-serial-consoles logo.nologo vt.global_cursor_default=0" >> /boot/cmdline.txt
-# TODO: Write script that replace "console=tty1" with "console=tty3"
+sudo sed -i 's/console=tty1/console=tty3/g' /boot/cmdline.txt
 
+
+sudo sed '/message_sprite/d' /usr/share/plymouth/themes/pix/pix.script
+sudo sed '/my_image/d' /usr/share/plymouth/themes/pix/pix.script
 
 #sudo nano /usr/share/plymouth/themes/pix/pix.script
 # Edit the plymouth pix.script, remove the following:
@@ -23,7 +28,6 @@ sudo echo "splash quiet plymouth.ignore-serial-consoles logo.nologo vt.global_cu
 #message_sprite.SetPosition(screen_width * 0.1, screen_height * 0.9, 10000);
 #my_image = Image.Text(text, 1, 1, 1);
 #message_sprite.SetImage(my_image);
-
 
 
 #
