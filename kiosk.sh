48d1cec2529fbcb161ede683776f05db0c45d126
Anders Vrethem <anders.vrethem@hiq.se>
2019-11-25 10:02:13 +0100

    Add kiosk script

---
+++ kiosk.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+export DISPLAY=":0"
+
+xset s noblank
+xset s off
+xset -dpms
+
+unclutter -idle 0.5 -root &
+
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
+
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk --ignore-certificate-errors --disable-restore-session-state https://www.latlmes.com/ & 
+
+while true; do
+   	WID=$(xdotool search --onlyvisible --class chromium|head -1)
+	xdotool windowactivate ${WID}
+	xdotool key ctrl+F
+   	sleep 15
+done
