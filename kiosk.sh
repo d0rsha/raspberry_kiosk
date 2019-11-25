1f6ccd8c844f5c789bc5f56fe92b4934616e8d16
Anders Vrethem <dersi_91@hotmail.com>
2019-11-25 15:36:42 +0100

    Update kiosk.sh

--- kiosk.sh
+++ kiosk.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-export XAUTHORITY=/home/pi/.Xauthority; export DISPLAY=:0; xdotool key Return
+set -x
+export XAUTHORITY=/home/pi/.Xauthority; export DISPLAY=:$DISPLAY; xdotool key Return
 
 xset s noblank
 xset s off
@@ -10,9 +11,10 @@ unclutter -idle 0.5 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
+sleep(30)
 /usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk --ignore-certificate-errors --disable-restore-session-state https://www.latlmes.com/breaking/your-sensational-news-headline-here-3 & 
 
 while true; do
 	xdotool key ctrl+F5
-   	sleep 15
+   	sleep(86400)
 done
