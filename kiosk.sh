1023b8cbc9150dc9e7405b7c7114d7ba8d366afa
Anders Vrethem <dersi_91@hotmail.com>
2019-11-26 10:36:24 +0100

    Update kiosk.sh

--- kiosk.sh
+++ kiosk.sh
@@ -12,9 +12,9 @@ sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromi
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
 # sleep 20s
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk --ignore-certificate-errors --disable-restore-session-state https://dashboard.projectngulia.org/board.html?workspace=ngulia#/live & 
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk --ignore-certificate-errors --disable-restore-session-state https://dashboard.projectngulia.org/board.html?workspace=ngulia#/live 
 
-while true; do
-	xdotool keydown ctrl+F5; xdotool keyup ctrl+F5;
-   	sleep 2m
-done
+#while true; do
+#	xdotool keydown ctrl+F5; xdotool keyup ctrl+F5;
+#   	sleep 2m
+#done
